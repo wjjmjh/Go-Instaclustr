aacb0b5e0ef682b1a36fd9f49afd6270c270719b
stephenma <stephen.ma@anu.edu.au>
2020-12-17 12:33:34 +1100

    BUILD: read config.json in main.

---
+++ main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"os"
+)
+
+type Configuration struct {
+	username      string
+	api_key       string
+	api_host_name string
+}
+
+func main() {
+	file, _ := os.Open("src/conf.json")
+	defer file.Close()
+	decoder := json.NewDecoder(file)
+	configuration := Configuration{}
+	err := decoder.Decode(&configuration)
+	if err != nil {
+		fmt.Println("error when parsing configuration file:", err)
+	}
+}
